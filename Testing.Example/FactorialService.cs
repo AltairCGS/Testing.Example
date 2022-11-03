9641e4c52098d1e9bccbafe19589365191bc5cd6
AltairCGS <jhangalvis422@gmail.com>
2022-11-03 11:01:23 -0500

    Agregar archivos de proyecto.

---
+++ Testing.Example/FactorialService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Example
+{
+    public class FactorialService
+    {
+        public int Calculate(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Calculate(number - 1);
+        }
+    }
+}
---
+++ Testing.ExampleTests/FactorialServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testing.Example;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Example.Tests
+{
+    [TestClass()]
+    public class FactorialServiceTests
+    {
+        [TestMethod()]
+        public void TestFactorialZero()
+        {
+            //Arrange
+            FactorialService service = new FactorialService();
+
+            //Act
+            int result = service.Calculate(0);
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestFactorialFive()
+        {
+            //Arrange
+            FactorialService service = new FactorialService();
+
+            //Act
+            int result = service.Calculate(5);
+
+            //Assert
+            Assert.AreEqual(120, result);
+        }
+    }
+}
